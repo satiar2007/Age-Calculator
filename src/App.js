3df10475172ae5ae7142cadae75e1d349954a07f
satiar2007 <snsatiar@gamil.com>
2024-10-08 22:56:59 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AgeCalator from './components/AgeCalculator';
+
+function App() {
+  return (
+    <div className="App">
+      <AgeCalator />
+    </div>
+  );
+}
+
+export default App;
